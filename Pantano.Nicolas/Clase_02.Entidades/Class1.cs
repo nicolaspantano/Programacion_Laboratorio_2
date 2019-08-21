edcc37d804231168ae90c66b26991932baf2396e
nicolaspantano <nicolaspantano_757@hotmail.com>
2019-08-21 12:00:44 -0300

    asd

---
+++ Pantano.Nicolas/Clase_02.Entidades/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Clase_02.Entidades
+{
+    public class Sello
+    {
+        public static string mensaje;
+        public static ConsoleColor color;
+        public static string Imprimir()
+        {
+            
+            return Sello.ArmarFormatoMensaje();
+        }
+
+        public static void Borrar()
+        {
+            Sello.mensaje = null;
+        }
+
+        public static void ImprimirEnColor()
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(Sello.ArmarFormatoMensaje());
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        static string ArmarFormatoMensaje()
+        {
+            string techo="";
+           
+           
+            for (int i = 0; i < mensaje.Length+2; i++)
+            {
+                techo += "*";
+            }
+            
+            return techo+"\n*"+mensaje+"*\n"+techo;
+        }
+    }
+}
--- Pantano.Nicolas/Clase_02/Program.cs
+++ Pantano.Nicolas/Clase_02/Program.cs
@@ -4,21 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Clase_02.Entidades;
 namespace Clase_02
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string name;
+            /*string name;
             MiClase.edad = 23;
             MiClase.nombre = "Juan";
             name = MiClase.RetornarNombre();
             Console.WriteLine(name);
-            MiClase.MostrarEdad();        
+            MiClase.MostrarEdad();  */
 
+            Sello.mensaje = "Hola mundo";
+            Console.WriteLine(Sello.Imprimir());
             
-            Console.ReadKey();
+            Sello.color = ConsoleColor.Green;
+            Sello.mensaje = "Hola mundo";
+            Sello.ImprimirEnColor();
+
+
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
