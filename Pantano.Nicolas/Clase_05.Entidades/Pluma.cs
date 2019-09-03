3d9c7e5e8a5ccca03a5e3c702d9f110150305e0f
nicolaspantano <nicolaspantano757@hotmail.com>
2019-09-03 11:50:35 -0300

    asd

---
+++ Pantano.Nicolas/Clase_05.Entidades/Pluma.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clase_05.Entidades
+{
+    class Pluma
+    {
+        private string marca;
+        private Tinta tinta;
+        private int cantidad;
+
+        public Pluma()
+        {
+            this.marca = "Sin marca";
+            this.tinta = null;
+            this.cantidad = 0;
+        }
+        public Pluma(string marca):this()
+        {
+            this.marca = marca;
+        }
+        public Pluma(string marca,Tinta tinta):this(marca)
+        {
+            this.tinta = tinta;
+        }
+        public Pluma(string marca, Tinta tinta, int cantidad) : this(marca, tinta)
+        {
+            this.cantidad = cantidad;
+        }
+
+        private string Mostrar()
+        {
+            return this.marca + " - " + (string)this.tinta + " - " + this.cantidad.ToString();
+        }
+
+        public static implicit operator string(Pluma pluma)
+        {
+            return pluma.Mostrar();
+        }
+        
+        public static bool operator ==(Pluma pluma, Tinta tinta)
+        {
+            return pluma.tinta == tinta;
+        }
+        
+        public static bool operator !=(Pluma pluma,Tinta tinta)
+        {
+            return !(pluma == tinta);
+        }
+        
+        public static Pluma operator +(Pluma pluma,Tinta tinta)
+        {
+            if (pluma == tinta&&pluma.cantidad<100)
+            {
+                pluma.cantidad++;
+            }
+            return pluma;
+        }
+    }
+}
---
+++ Pantano.Nicolas/Clase_05.Entidades/Tinta.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Clase_05.Entidades
+{
+    public class Tinta
+    {
+        private ConsoleColor color;
+        private ETipoTinta tipo;
+
+        public Tinta()
+        {
+            this.color = ConsoleColor.Blue;
+            this.tipo = ETipoTinta.Comun;
+        }
+
+        public Tinta(ConsoleColor color) : this()
+        {
+            this.color = color;
+        }
+
+        public Tinta(ConsoleColor color, ETipoTinta tipo) : this(color)
+        {
+            this.tipo = tipo;
+        }
+
+        private string Mostrar()
+        {
+            return this.color.ToString() + " - " + this.tipo.ToString();
+        }
+
+        public static string Mostrar(Tinta tinta)
+        {
+            return tinta.Mostrar();
+        }
+
+        public static bool operator ==(Tinta tinta1,Tinta tinta2)
+        {
+            if(Object.Equals(tinta1,null)||Object.Equals(tinta2,null))
+            {                                
+                    return false;                               
+            }
+            else if (tinta1.color == tinta2.color && tinta2.tipo == tinta1.tipo)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+               
+            
+            
+        }
+        
+        public static bool operator !=(Tinta tinta1,Tinta tinta2)
+        {
+            return !(tinta1 == tinta2);
+        }
+
+        public static bool operator ==(Tinta tinta,ConsoleColor color)
+        {
+            if (tinta.color == color)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(Tinta tinta,ConsoleColor color)
+        {
+            return !(tinta == color);
+        }
+
+        public static explicit operator string (Tinta tinta)
+        {
+            return tinta.Mostrar();
+        }
+    }
+}
