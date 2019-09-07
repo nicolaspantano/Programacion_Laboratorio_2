e29e125c67cac173b477e13c4c5eae963c6ec1d8
NicolasPantano <nicolaspantano_757@hotmail.com>
2019-09-07 19:53:41 -0300

    Ejercicio 13 hecho

---
+++ Pantano.Nicolas/Ejercicio_13/Conversor.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_13
+{
+    class Conversor
+    {
+        public static string DecimalBinario(double doble)
+        {
+            string binario="";
+
+            while(doble>1)
+            {
+                if (doble % 2 == 0)
+                {
+                    binario = "0" + binario;
+                }
+                else
+                {
+                    binario = "1" + binario;
+                }
+
+                doble = doble / 2; 
+            }
+
+
+            return binario;
+        }
+
+        public static double BinarioDecimal(string binario)
+        {
+            double doble=0;
+            
+            int exponente = 0;
+
+            
+           
+            for (int i = (binario.Length)-1; i>=0; i--)
+            {
+                               
+                if (binario[i]=='1')
+                {
+                    if(i==(binario.Length)-1)
+                    {
+                        doble += 1;
+                        continue;
+                       
+                    }
+                    else
+                    {
+                        doble +=Math.Pow(2,exponente);
+                        
+                    }
+                    
+                }
+
+                exponente++;
+
+            }
+
+            
+            return doble;
+
+        }
+    }
+}
+
+
+
+
+    
+
---
+++ Pantano.Nicolas/Ejercicio_13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double doble = 120;
+
+            Console.WriteLine(Conversor.DecimalBinario(doble));
+
+            string binario = "1100110";
+
+            Console.WriteLine(Conversor.BinarioDecimal(binario));
+
+            Console.ReadKey();
+
+           
+        }
+    }
+}
