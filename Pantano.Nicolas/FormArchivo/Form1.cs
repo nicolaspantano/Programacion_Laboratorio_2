9d39181ed92ad8e65538e4abaa57abea8a700902
alumno <alumno@utnfra.com.ar>
2019-10-16 10:51:48 -0300

    asd

--- Pantano.Nicolas/FormArchivo/Form1.cs
+++ Pantano.Nicolas/FormArchivo/Form1.cs
@@ -13,21 +13,32 @@ namespace FormArchivo
 {
     public partial class Form1 : Form
     {
+        DirectoryInfo di = new DirectoryInfo(@"C:\Users\" + Environment.UserName + @"\");
+
         public Form1()
         {
             InitializeComponent();
 
-            foreach(Environment.SpecialFolder item in Enum.GetValues(typeof(Environment.SpecialFolder)))
+            /*foreach(Environment.SpecialFolder item in Enum.GetValues(typeof(Environment.SpecialFolder)))
             {
                 this.cmbUbicacion.Items.Add(item);
+            }*/
+
+            foreach(var direc in di.GetDirectories())
+            {
+                cmbUbicacion.Items.Add(direc);
             }
+                        
+            
         }
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
             try
             {
-                StreamWriter writer = new StreamWriter(@"C:\Users\alumno\" +this.cmbUbicacion.SelectedItem.ToString() +@"\"+this.txtNombre.Text,false);
+                //StreamWriter writer = new StreamWriter(Environment.GetFolderPath((Environment.SpecialFolder)this.cmbUbicacion.SelectedItem) + @"\" + this.txtNombre.Text, false);
+                StreamWriter writer = new StreamWriter(di.ToString() + cmbUbicacion.SelectedItem.ToString() + @"\" + this.txtNombre.Text + ".txt", false);
+
                 writer.WriteLine(txtTexto.Text);
                 writer.Close();
                
@@ -42,7 +53,9 @@ namespace FormArchivo
         {
             try
             {
-                StreamReader reader = new StreamReader(@"C:\Users\alumno\" + this.cmbUbicacion.SelectedItem.ToString() + @"\" + this.txtNombre.Text, false);
+                //StreamReader reader = new StreamReader(Environment.GetFolderPath((Environment.SpecialFolder)this.cmbUbicacion.SelectedItem) + @"\" + this.txtNombre.Text, false);
+                StreamReader reader = new StreamReader(di.ToString() +cmbUbicacion.SelectedItem.ToString()+@"\"+ this.txtNombre.Text+ ".txt", false);
+
                 this.txtTexto.Text = reader.ReadToEnd();
                 reader.Close();
             }
--- Pantano.Nicolas/TestDeposito/Program.cs
+++ Pantano.Nicolas/TestDeposito/Program.cs
@@ -11,11 +11,21 @@ namespace TestDeposito
     {
         static void Main(string[] args)
         {
+            Cocina c1 = new Cocina(111, 12300, false);
+            Cocina c2 = new Cocina(112, 15000, true);
+            Cocina c3 = new Cocina(113, 5600, false);
             Auto a1 = new Auto("Rojo", "Ferrari");
             Auto a2 = new Auto("Amarillo", "Porche");
             Auto a3 = new Auto("Negro", "BMW");
             Auto a4 = new Auto("Verde", "Ford");
-            DepositoDeAutos da = new DepositoDeAutos(3);
+            Deposito<Cocina> dc = new Deposito<Cocina>(5);
+            Deposito<Auto> da = new Deposito<Auto>(3);
+            dc.Agregar(c1);
+            dc.Agregar(c2);
+            if (!(dc + c3))
+            {
+                Console.WriteLine("No se pudo agregar el item!!!");
+            }
             if ((da + a1))
             {
                 Console.WriteLine("Se ha agregado el item!!!");
@@ -26,14 +36,23 @@ namespace TestDeposito
             {
                 Console.WriteLine("No se pudo agregar el item!!!");
             }
+            Console.WriteLine(dc);
             Console.WriteLine(da);
-            da.Remover(a2);
-            if (!(da - a4))
-{
+            dc.Remover(c2);
+            if (!(dc - c2))
+            {
                 Console.WriteLine("No se pudo remover el item!!!");
             }
+            da.Remover(a2);
+            if (!(da - a4))
+            {
+                Console.WriteLine("No se pudo remover el item!!!");
+            }
+            Console.WriteLine(dc);
             Console.WriteLine(da);
+            
             Console.ReadLine();
+
         }
     }
 }
